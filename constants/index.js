bcbb8482d15fd828cc75455a8783d7f1b5632aea
lutherdev <devturd@gmail.com>
2025-08-22 21:47:10 +0800

    feat: added navbar

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+export const navLinks = [
+    {id: 'balls', title: 'pool balls'},
+    {id: 'sticks', title: 'pool sticks'},
+    {id: 'about', title: 'About Us'},
+]
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import {navLinks} from '../../constants/index.js'
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <p>POOL</p>
+                </a>
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={'#${link.id}'}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
